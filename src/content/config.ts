a2b3d97f01c363fbafb9254d8cd5e838b5dcf31c
Guirdo <aldo.seb.mendez@gmail.com>
2024-06-29 15:16:45 -0600

    🗃️ Add Blog schema

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+        author: z.string(),
+        image: z.string(),
+        imageAlt: z.string(),
+        tags: z.array(z.string()),
+    }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
